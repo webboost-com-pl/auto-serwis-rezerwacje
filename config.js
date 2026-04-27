19cb3b3357dda36425e46686df71a68b77b1bac4
webboost-com-pl <kontakt.webboost@gmail.com>
2026-04-27 19:08:30 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://sdpxqugrvssnkesjfkh y.supabase.co";
+const SUPABASE_URL = "https://sdpxqugrvssnkesjfkhy.supabase.co";
 const SUPABASE_KEY = "sb_publishable_FoFzvVpIn00A6wjsXlxHdw_K2hDZpYh";
 
 var supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
